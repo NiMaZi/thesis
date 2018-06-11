b3c00738cd21c1600034d0bd09ac948617de05e7
NiMaZi <zhengyl940425@gmail.com>
2018-06-12 01:46:15 +0200

    f

--- models/LDA_test.py
+++ models/LDA_test.py
@@ -19,20 +19,19 @@ tfidf=TfidfModel.load(homedir+"/results/models/tfidf_model")
 
 for i in [5,10,20,25,40,50,100,200,250,400,500,1000]:
     lda=AuthorTopicModel(id2word=dictionary.id2token,num_topics=i,eval_every=False)
-    for j in range(0,19,4):
-        f=open(homedir+"/thesiswork/source/corpus/lda_doc_part"+str(j)+".json",'r')
-        _corpus=json.load(f)
-        f.close()
-        bow_corpus=[dictionary.doc2bow(doc) for doc in _corpus]
-        tfidf_corpus=[tfidf[doc] for doc in bow_corpus]
-        f=open(homedir+"/thesiswork/source/corpus/lda_d2a_part"+str(j)+".json",'r')
-        _d2a=json.load(f)
-        f.close()
-        d2a={}
-        for k in _d2a.keys():
-            d2a[int(k)]=_d2a[k]
-        lda.update(corpus=tfidf_corpus,doc2author=d2a)
-        lf=open(homedir+"/results/logs/lda_training_log.txt",'a')
-        lf.write("finish training %d-%d\n"%(i,j))
-        lf.close()
-    lda.save(homedir+"/results/models/lda20000_topic"+str(i))
+    f=open(homedir+"/thesiswork/source/corpus/lda_doc_2k.json",'r')
+    _corpus=json.load(f)
+    f.close()
+    bow_corpus=[dictionary.doc2bow(doc) for doc in _corpus]
+    tfidf_corpus=[tfidf[doc] for doc in bow_corpus]
+    f=open(homedir+"/thesiswork/source/corpus/lda_d2a_2k.json",'r')
+    _d2a=json.load(f)
+    f.close()
+    d2a={}
+    for k in _d2a.keys():
+        d2a[int(k)]=_d2a[k]
+    lda.update(corpus=tfidf_corpus,doc2author=d2a)
+    lf=open(homedir+"/results/logs/lda_training_log.txt",'a')
+    lf.write("finish training "+str(i)+" topics.\n")
+    lf.close()
+    lda.save(homedir+"/results/models/lda2000_body_topic"+str(i))
