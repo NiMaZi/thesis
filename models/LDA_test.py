5aecd3614065e680cc3bc8222061144a5e21cbdd
NiMaZi <zhengyl940425@gmail.com>
2018-06-12 00:50:59 +0200

    f

--- models/LDA_test.py
+++ models/LDA_test.py
@@ -26,9 +26,12 @@ for i in [5,10,20,25,40,50,100,200,250,400,500,1000]:
         bow_corpus=[dictionary.doc2bow(doc) for doc in _corpus]
         tfidf_corpus=[tfidf[doc] for doc in bow_corpus]
         f=open(homedir+"/thesiswork/source/corpus/lda_d2a_part"+str(j)+".json",'r')
-        d2a=json.load(f)
+        _d2a=json.load(f)
         f.close()
-        lda=AuthorTopicModel(corpus=tfidf_corpus,doc2author=d2a)
+        d2a={}
+        for k in _d2a.keys():
+            d2a[int(k)]=_d2a[k]
+        lda.update(corpus=tfidf_corpus,doc2author=d2a)
         f.open(homedir+"/results/logs/lda_training_log.txt",'a')
         f.write("finish training %d-%d\n"%(i,j))
         f.close()
