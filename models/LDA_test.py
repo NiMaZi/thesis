6752850a51f9cbeb53f380d84d988a9b5efbc0f3
NiMaZi <zhengyl940425@gmail.com>
2018-06-12 00:42:20 +0200

    json

--- models/LDA_test.py
+++ models/LDA_test.py
@@ -20,12 +20,12 @@ tfidf=TfidfModel.load(homedir+"/results/models/tfidf_model")
 for i in [5,10,20,25,40,50,100,200,250,400,500,1000]:
     lda=AuthorTopicModel(id2word=dictionary.id2token,num_topics=i,eval_every=False)
     for j in range(0,19):
-        f=open(homedir+"/thesiswork/source/corpus/lda_doc_part"+str(j),'r')
+        f=open(homedir+"/thesiswork/source/corpus/lda_doc_part"+str(j)+".json",'r')
         _corpus=json.load(f)
         f.close()
         bow_corpus=[dictionary.doc2bow(doc) for doc in _corpus]
         tfidf_corpus=[tfidf[doc] for doc in bow_corpus]
-        f=open(homedir+"/thesiswork/source/corpus/lda_d2a_part"+str(j),'r')
+        f=open(homedir+"/thesiswork/source/corpus/lda_d2a_part"+str(j)+".json",'r')
         d2a=json.load(f)
         f.close()
         lda=AuthorTopicModel(corpus=tfidf_corpus,doc2author=d2a)
