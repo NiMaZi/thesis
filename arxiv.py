6a22be7bf480887cce0d150241964c308194cb9e
NiMaZi <zhengyl940425@gmail.com>
2018-06-24 15:47:32 +0200

    get

--- arxiv.py
+++ arxiv.py
@@ -2,5 +2,5 @@ import os
 import subprocess
 
 for i in range(21,32):
-	subprocess.call(['s3cmd','--requester-pays','s3://arxiv/pdf/arXiv_pdf_1805_0'+str(i)+'.tar'])
+	subprocess.call(['s3cmd','get','--requester-pays','s3://arxiv/pdf/arXiv_pdf_1805_0'+str(i)+'.tar'])
 	subprocess.call(['tar','-xf','arXiv_pdf_1805_0'+str(i)+'.tar'])
